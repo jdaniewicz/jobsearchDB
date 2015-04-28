51656ab36923d1dc07d494a5b14bc912c7bf7c78
jdaniewicz <Jeffrey.daniewicz@uconn.edu>
2015-04-27 23:17:45 -0400

    Got rid of accidental inner copy

--- uconnjobsearch/uconnjobsearch/templates/hello.php
+++
@@ -1,7 +0,0 @@
-<?php include("/layouts/generalHeader.php"); ?>
-
- <h1> 
- 	WRONG PASSWORD!
-  </h1>
-
-<?php include("/layouts/generalFooter.php"); ?>
--- uconnjobsearch/uconnjobsearch/templates/loggedout.php
+++
@@ -1,8 +0,0 @@
-<?php include("/layouts/generalHeader.php"); ?>
-
- <h1> 
- 	You are not logged in.
-  </h1>
-  <a href="/uconnjobsearch/login">Log in Here</a>
-
-<?php include("/layouts/generalFooter.php"); ?>
